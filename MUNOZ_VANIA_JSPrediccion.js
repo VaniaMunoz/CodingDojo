74a9842aea0cb9aa021221a8dfc0e3b9a7fc3994
VaniaMunoz <vania.munoz.anartes@alumnos.sip.cl>
2022-04-07 11:43:53 -0400

    coding

---
+++ MUNOZ_VANIA_JSPrediccion.js
@@ -0,0 +1,21 @@
+functioncopy myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}
+myBirthYearFunc (1980)
+"Nací en 1980" El console.log me indica en que año nací
+
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+}
+myBirthYearFunc(1980)
+"Nací en 1980" Indica el año  de EntradaAñoNacimiento
+
+function add(num1, num2){
+        console.log("¡Sumando números!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+}
+console.log(20) Me indica el resultado de la suma de los numeros
