88fa533f428f217aca763fb24d81769139056213
VaniaMunoz <vania.munoz.anartes@alumnos.sip.cl>
2022-04-12 11:07:52 -0400

    DiagramaenT

---
+++ vania.js
@@ -0,0 +1,40 @@
+/*
+Fragmento de código 1
+*/
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+    Variables  Datos
+    produce    =   ["manzanas", "naranjas"];
+    /*
+    Variables   Datos
+    auntContactInfo     =   ["paula", "Smith", "Calle Principal 1234", "St. Louis, "Mo", 12345];
+    */
+}
+/*
+
+Fragmento de código 2
+*/
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+    /*
+    Variables   Datos
+    produce     =   ["manzanas", "naranjas"];
+    frozen      =   ["brocoli", "helado"];
+    */
+}
+/*
+
+Fragmento de código 3
+*/
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+/*
+    Variables   Datos
+    movieLibrary    =   ["Bambi", "E:T", "Toy Story"];
+/*
