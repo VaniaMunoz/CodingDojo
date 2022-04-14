37f80c674ed29f699b129ebf699b20c77e03a1bd
VaniaMunoz <vania.munoz.anartes@alumnos.sip.cl>
2022-04-14 10:26:05 -0400

    Return

--- vania_return.js
+++ vania_return.js
@@ -18,7 +18,7 @@ var result = hello();
 console.log('result is', result);
 /*
     Variables   Datos
-    result  =   hello,result is 15
+    result  =   hello(),result is 15
 
 */
 
@@ -31,7 +31,7 @@ var result = numPlus(3);
 console.log('result is', result);
 /*
     Variables Datos
-    result  =   num is 3 ,result is 18
+    result  =   numPlus(3); =   num is 3 ,result is 18
 
 */
 
@@ -47,7 +47,8 @@ console.log(result);
 console.log(num);<div id="copy-toolbar-container" style="cursor: pointer; position: absolute; top: 5px; right: 5px; padding: 0px 3px; background: rgba(224, 224, 224, 0.2); box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 0px 0px; color: rgb(187, 187, 187); border-radius: 0.5em; font-size: 0.8em;"><span id="copy-toolbar">copy</span></div>
 /*
     variables datos
-    result  =   [15,10,10,15]
+    num     =   15;
+    result  =   logAndReturn(10);   =   [15,10,10,15]
 */
 
 Fragmento de código 5
@@ -62,7 +63,8 @@ console.log(result);
 console.log(num);
 /*
     variables datos
-    result  =   [15,10,20,15]
+    num =   15;
+    result  =  timesTwo(10); =  [15,10,20,15]
 */
 
 
@@ -76,7 +78,8 @@ var result = timesTwoAgain(3) + timesTwoAgain(5);
 console.log('result is', result);
 /*
     variables datos
-    result  = result is 16 ,num is 3,num is 5
+    timesTwoAgain(3) + timesTwoAgain(5) [result is 16] 
+    result  = ,num is 3,num is 5
 */
 
 
@@ -114,7 +117,8 @@ var result = sumNums(2,3) + sumNums(3,5);
 console.log('result is', result);
 /*
     variables datos
-    result  =   result is 13 ,sum is 5, sum is 8
+    sum     =   num1 + num2;
+    result  =   sumNums(2,3) + sumNums(3,5); result is 13 ,sum is 5, sum is 8
 */
 
 
@@ -128,5 +132,7 @@ var result = sumNums(2,3) + sumNums(3,sumNums(2,1)) + sumNums(sumNums(2,1),sumNu
 console.log('result is', result);
 /*
     variables datos 
-    result  =   result is 19,sum is 5,sum is 3,sum is 6,sum is 3,sum is 5,sum is 8
+    sum     =   num1 + num2;
+    result  =   sumNums(2,3) + sumNums(3,sumNums(2,1)) + sumNums(sumNums(2,1),sumNums(2,3));
+    
 */
