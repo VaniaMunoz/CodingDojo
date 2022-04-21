882c4aed9de447e0c9e70ccba0d0293adba6eb58
VaniaMunoz <vania.munoz.anartes@alumnos.sip.cl>
2022-04-21 09:35:30 -0400

    esta vez .js

---
+++ Munoz_Vania_JS_Jugueteo.js
@@ -0,0 +1,26 @@
+function muestraSiElNiñoPuedeSubirALaMontañaRusa (alturaNiño) {
+    if (alturaNiño >= 52) {
+      console.log ("¡Súbete, chico!")
+  }
+  else {
+  //  alturaniño < 52; {
+      console.log ("lo siento chico, tal vez el proximo año.")
+   // }
+  }
+  }
+  muestraSiElNiñoPuedeSubirALaMontañaRusa(50);
+  
+  function muestraSiElNiñoPuedeSubirALaMontañaRusa (alturaNiño) {
+    if (alturaNiño >= 58) {
+      console.log ("¡Súbete, chico!")
+  }
+  else {
+  //  alturaniño < 58; {
+      console.log ("¡Súbete, chico!")
+   // }
+  }
+  }
+  muestraSiElNiñoPuedeSubirALaMontañaRusa(58);
+
+
+   
